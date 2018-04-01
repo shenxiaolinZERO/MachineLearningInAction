93afbd83fec18f059f1980a81295c722dfd069b1
shenxiaolinZERO <2311513185@qq.com>
2018-04-01 22:52:20 +0800

    LinearRegression

---
+++ Ch04NaiveBayes/NaiveBayes.py
@@ -0,0 +1,4 @@
+# !/usr/bin/env python
+# encoding: utf-8
+__author__ = 'Administrator'
+# Naive Bayes 朴素贝叶斯
---
+++ Ch08LinearRegression/LinearRegression.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python
+# encoding: utf-8
+__author__ = 'Administrator'
+
+#回归的一般方法：
+#（1）收集数据
+#（2）准备数据
+#（3）分析数据
+#（4）训练算法
+#（5）测试算法
+#（6）使用算法
+
+
+
+
