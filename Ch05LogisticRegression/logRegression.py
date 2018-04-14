4d6266a5d00a811ac6cdec25f651ec7bbcc8801c
shenxiaolinZERO <2311513185@qq.com>
2018-04-14 15:39:54 +0800

    MLCourse..Ch05LogRegre-plotBsetFit

--- Ch05LogisticRegression/logRegression.py
+++ Ch05LogisticRegression/logRegression.py
@@ -4,8 +4,8 @@ __author__ = 'Administrator'
 # 20180414 Sat
 
 from math import *
-import numpy as np
 from numpy import *
+import matplotlib.pyplot as plt
 
 #打开文本文件并逐行读取
 def loadDataSet():
@@ -35,6 +35,31 @@ def gradAscent(dataMatIn, classLabels):
         weights=weights+alpha * dataMatrix.transpose() * error
     return weights
 
+#画出数据集合 Logistic回归最佳拟合曲线
+def plotBestFit(wei):
+    weights=wei.getA()
+    dataMat,labelMat=loadDataSet()
+    dataArr=array(dataMat)
+    n=shape(dataArr)[0]
+    xcord1=[]; ycord1=[]
+    xcord2=[]; ycord2=[]
+    for i in range(n):
+        if int(labelMat[i])==1:
+            xcord1.append(dataArr[i,1]) ; ycord1.append(dataArr[i,2])
+        else:
+            xcord2.append(dataArr[i,1]) ; ycord2.append(dataArr[i,2])
+    fig=plt.figure()
+    ax=fig.add_subplot(111)
+    ax.scatter(xcord1,ycord1,s=30,c='red',marker='s')
+    ax.scatter(xcord2,ycord2,s=30,c='green')
+    x=arange(-3.0,3.0,0.1)
+    y=(-weights[0]-weights[1]*x)/weights[2]
+    ax.plot(x,y)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+
+
 if __name__ == '__main__':
     dataArr,labelMat=loadDataSet()
     weights=gradAscent(dataArr,labelMat)
@@ -42,5 +67,5 @@ if __name__ == '__main__':
     # [[4.12414349]
     #  [0.48007329]
     #  [-0.6168482]]
-
+    plotBestFit(weights)
 
