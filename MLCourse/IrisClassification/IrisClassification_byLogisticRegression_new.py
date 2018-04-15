d994d99493d9866fe349e77740334770468d1688
shenxiaolinZERO <2311513185@qq.com>
2018-04-15 11:30:44 +0800

    MLCourse..LR_new

--- MLCourse/IrisClassification/IrisClassification_byLogisticRegression_new.py
+++ MLCourse/IrisClassification/IrisClassification_byLogisticRegression_new.py
@@ -159,3 +159,36 @@ if __name__ == '__main__':
     # LogRegressionAlgorithm()
     rights, predict, kinds=LogRegressionAlgorithm()
     confusion_matrix(rights, predict, kinds)
+    
+    # (5)绘制图像
+    # 1.确定坐标轴范围，x，y轴分别表示两个特征
+    x1_min, x1_max = x[:, 0].min(), x[:, 0].max()  # 第0列的范围
+    x2_min, x2_max = x[:, 1].min(), x[:, 1].max()  # 第1列的范围
+    x1, x2 = np.mgrid[x1_min:x1_max:200j, x2_min:x2_max:200j]  # 生成网格采样点
+    grid_test = np.stack((x1.flat, x2.flat), axis=1)  # 测试点
+    # print 'grid_test = \n', grid_test
+    grid_hat = classifier.predict(grid_test)       # 预测分类值
+    grid_hat = grid_hat.reshape(x1.shape)  # 使之与输入的形状相同
+
+
+    # 2.指定默认字体
+    mpl.rcParams['font.sans-serif'] = [u'SimHei']
+    mpl.rcParams['axes.unicode_minus'] = False
+
+    # 3.绘制
+    cm_light = mpl.colors.ListedColormap(['#A0FFA0', '#FFA0A0', '#A0A0FF'])
+    cm_dark = mpl.colors.ListedColormap(['g', 'r', 'b'])
+
+    alpha=0.5
+
+    plt.pcolormesh(x1, x2, grid_hat, cmap=cm_light) # 预测值的显示
+    # plt.scatter(x[:, 0], x[:, 1], c=y, edgecolors='k', s=50, cmap=cm_dark)  # 样本
+    plt.plot(x[:, 0], x[:, 1], 'o', alpha=alpha, color='blue', markeredgecolor='k')
+    plt.scatter(x_test[:, 0], x_test[:, 1], s=120, facecolors='none', zorder=10)  # 圈中测试集样本
+    plt.xlabel(u'花萼长度', fontsize=13)
+    plt.ylabel(u'花萼宽度', fontsize=13)
+    plt.xlim(x1_min, x1_max)
+    plt.ylim(x2_min, x2_max)
+    plt.title(u'鸢尾花SVM二特征分类', fontsize=15)
+    # plt.grid()
+    plt.show()
