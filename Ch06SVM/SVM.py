e9653ec91993621778942ce1c6cec764d9148395
shenxiaolinZERO <2311513185@qq.com>
2018-05-10 13:24:32 +0800

    SVM-Platt-SMO-assistant-func2-codes

--- Ch06SVM/SVM.py
+++ Ch06SVM/SVM.py
@@ -64,12 +64,27 @@ __author__ = 'Administrator'
 #  同时，我们也需要另一个辅助函数，用于在数值太大时对其进行调整。
 
 # SMO算法中的辅助函数
+import random
 def loadDataSet(fileName):
     dataMat=[]; labelMat= []
     fr=open(fileName)
     for line in fr.readlines():
         lineArr =line.strip().split('\t')
-        
+        dataMat.append([float(lineArr[0]),float(lineArr[1])])
+        labelMat.append(float(lineArr[2]))
+    return dataMat,labelMat
+def selectJrand(i,m):
+    j=i
+    while (j==i):
+        j = int(random.uniform(0,m))
+    return j
+def clipAlpha(aj,H,L):
+    if aj > H:
+        aj=H
+    if L > aj:
+        aj=L
+    return aj
+
 
 
 
