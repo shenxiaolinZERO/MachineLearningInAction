06208ae15cb311d23c7ee743c31dcf53dc8cbfab
shenxiaolinZERO <2311513185@qq.com>
2018-06-15 11:13:29 +0800

    MLInAction-SVM-simpleSMO-" git commit  -m MLInAction-SVM-simpleSMO-"

--- Ch06SVM/SVM.py
+++ Ch06SVM/SVM.py
@@ -108,7 +108,9 @@ def smoSimple(dataMatIn,classLabels,C,toler,maxIter):
                 alphaIold = alphas[i].copy()
                 alphaJold = alphas[j].copy()
                 if (labelMat[i] != labelMat[j]):
-                    
+                    L=max(0,alphas[j]-alphas[i])
+                    H=min(C,C+alphas[j]-alphas[i])
+
 
     return 0
 
