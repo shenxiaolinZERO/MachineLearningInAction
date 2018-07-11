b6129c41399347dd92519cec600de416fbb4d46b
shenxiaolinZERO <2311513185@qq.com>
2018-07-11 11:03:24 +0800

    Explicit-BTL、

--- Ch06SVM/SVM2_optStruct.py
+++ Ch06SVM/SVM2_optStruct.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 __author__ = 'Administrator'
 
-
+from numpy import *
 # 20180711
 # ~
 # 2018...
@@ -10,4 +10,6 @@ class optStruct:
     def __init__(self,dataMatIn,classLabels,C,toler):
         self.X = dataMatIn
         self.labelMat = classLabels
-        
+        self.C = C
+        self.tol = toler
+        self.m = shape(dataMatIn)[0]
