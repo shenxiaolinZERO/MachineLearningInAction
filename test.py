c96ad2ce21a5e453fff2ff1aa6502ce5e0cd2c01
shenxiaolinZERO <2311513185@qq.com>
2018-07-21 20:05:51 +0800

    nowercode

--- test.py
+++ test.py
@@ -46,19 +46,6 @@ class Person(object):
         return '<Person: %s(%s)>' %(self.name,self.age)
 
 
-def SumOfLqa():
-    inputIntegers = input("Please input 6 positive integers[<100],the first integer is a： ")
-    list = inputIntegers.split()
-    a = list[0]
-    sum = 0
-    for i in range(length(list)):
-        if list[i+1] < a:
-            sum += list[i+1]
-    print(sum )
-    return sum
-
-
 if __name__ == '__main__':
-    # zhangsan = Person('zhangsan',24)
-    # print(zhangsan)
-    SumOfLqa()
+    zhangsan = Person('zhangsan',24)
+    print(zhangsan)
